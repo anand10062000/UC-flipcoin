cea2157dd2ace6c9a3ce91bed4b43bd3afeaadcc
anand10062000 <anandkumarramisetti@gmail.com>
2021-08-22 21:43:53 +0530

    number of flpis

--- flipc.sh
+++ flipc.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 FLIP=$((RANDOM%2))
-if [ $FLIP -eq 1 ]
-then 
-echo "head"
-else
-echo "tail"
+echo $FLIP
+head=0
+for ((i=0;i<10;i++))
+do
+if [ $FLIP -eq 1 ];then
+    echo "heads"
+    i=$(($i+1));
+     count >>$head
+elif [ $FLIP -eq 0 ]
+then
+    echo "tails"
+    i=$(($i+1));
 fi
+done
 
